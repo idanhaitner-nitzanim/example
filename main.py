c91b5017ab8a1efc0a736f601552aaff70a66656
idanhaitner-nitzanim <idan.haitner@nitzanim.tech>
2026-01-12 19:10:10 +0200

    delete all

--- main.py
+++
@@ -1,9 +0,0 @@
-from helpers import *
-from consts.math import PI, E
-num1 = int(input("Enter a number"))
-num2 = int(input("Enter a number"))
-operation = input("operation")
-print(PI)
-if operation == "+":
-    print(pow())
-
