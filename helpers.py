fff53f81311df913be0bede518450275c566dc42
idanhaitner-nitzanim <idan.haitner@nitzanim.tech>
2026-01-12 19:04:20 +0200

    draft

---
+++ helpers.py
@@ -0,0 +1,21 @@
+def add(num1, num2):
+    return num1 + num2
+
+
+def sub(num1, num2):
+    return num1 - num2
+
+
+def mult(num1, num2):
+    return num1 * num2
+
+
+def pow(num1, num2):
+    return num1 ** num2
+
+
+def div(num1, num2):
+    if (num2 == 0):
+        print("Cannot divide by zero")
+    else:
+        return num1 / num2
--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-# This is a sample Python script.
+from helpers import *
+from consts.math import PI, E
+num1 = int(input("Enter a number"))
+num2 = int(input("Enter a number"))
+operation = input("operation")
+print(PI)
+if operation == "+":
+    print(pow())
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
